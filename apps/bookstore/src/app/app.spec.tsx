53d9bb191e1546fde7bb3d210fb9cc52466676a9
galihasmarandaru <galih.asmarandaru@gmail.com>
2022-04-22 19:29:24 +0700

    end of chapter one

--- apps/bookstore/src/app/app.spec.tsx
+++ apps/bookstore/src/app/app.spec.tsx
@@ -1,8 +1,9 @@
-import { render } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 
 import App from './app';
 
 describe('App', () => {
+  afterEach(cleanup);
   it('should render successfully', () => {
     const { baseElement } = render(<App />);
 
@@ -12,6 +13,6 @@ describe('App', () => {
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />);
 
-    expect(getByText(/Welcome bookstore/gi)).toBeTruthy();
+    expect(getByText('Bookstore')).toBeTruthy();
   });
 });
